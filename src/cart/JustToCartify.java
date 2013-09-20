6ceb6a5614bd1e4616bee2eef4b406c741830b80
M. Emin Aksehirli <emin.aksehirli@gmail.com>
2013-09-20 17:38:09 +0200

    Copyright notices are added to CartiClus files.

--- src/cart/JustToCartify.java
+++ src/cart/JustToCartify.java
@@ -1,3 +1,22 @@
+/* The class that drives the Cartifier.
+ *  
+ * Copyright (C) 2011 - 2013  Emin Aksehirli, Sandy Moens
+ *
+ * This file is part of CartiClus - http://adrem.ua.ac.be/cartification . 
+ * 
+ * MIME is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package cart;
 
 import java.io.BufferedInputStream;
@@ -17,8 +36,6 @@ import java.util.zip.GZIPInputStream;
 
 public class JustToCartify
 {
-	// private static final int N_THREADS = 4;
-
 	public static void main(String[] args) throws IOException
 	{
 
